ff0348522e968ea6cb37cf13a6119937268ffc5f
KaidenJosephKeane <kaiden.keane@stu.vertexacademies.org>
2024-10-16 14:11:50 -0400

    Add files via upload

---
+++ Kaiden_Keane_JS_02.js
@@ -0,0 +1,13 @@
+// Example:             // Expected Output
+console.log(8**2) // 64
+console.log(60-8) // 52
+console.log(9*8) // 72
+console.log(66+5) // 71
+console.log(50*10) // 500
+// Write 5 more expressions below:
+console.log(!true);
+console.log(!!false);
+console.log(true || true);
+console.log(false || true);
+console.log(true || true)
+console.log(false || true);
